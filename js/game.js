39a6e453e04eba01840c60279ef3240a9441c26a
Damian de Vega <damiandevega@gmail.com>
2017-07-23 08:35:09 -0700

    Pause Menu and Score

--- js/game.js
+++ js/game.js
@@ -13,16 +13,21 @@
             }
     );
 
+    var w = 600, h = 400;
+
     function preload() {
         // Load the spritesheet 'character.png', telling Phaser each frame is 40x64
-        game.load.spritesheet('character', 'assets/images/lukeSpriteSheet.png', 32, 41);
-        game.load.image('coin', "assets/images/coin.png");
+        game.load.spritesheet('character', 'assets/images/spritesheet_luke.png', 32, 41);
+        game.load.spritesheet('coin', "assets/images/spritesheet_coin.png", 32, 32);
 
         // game.load.tilemap('map', 'assets/tilemaps/maps/tatooine3.json', null, Phaser.Tilemap.TILED_JSON);
         // game.load.image('gold', 'assets/tilemaps/tiles/gold.png');
         game.load.tilemap('map', 'assets/tilemaps/maps/background_test.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('desertTile', "assets/tilemaps/tiles/desert_tile.png");
         game.load.image('bluesky', "assets/tilemaps/tiles/bluesky.png");
+
+        game.load.image('ice', 'assets/images/block-ice.png');
+        game.load.image('menu', 'assets/images/number-buttons-90x90.png', 270, 180);
 
     }
 
@@ -104,9 +109,95 @@
 
             //  This just gives each star a slightly random bounce value
             coin.body.bounce.y = 0.7 + Math.random() * 0.2;
+
+            coin.animations.add('coin',[0,1,2,3,4,5], 15, true);
+
+            coin.animations.play('coin');
         }
 
+        /*
+            Code from example diamond burst
+        */
+        game.stage.backgroundColor = '#337799';
+        emitter = game.add.emitter(game.world.centerX, 100, 200);
+        emitter.makeParticles('ice');
+        emitter.start(false, 5000, 20);
+
+        scoreText = game.add.text(16, 15, 'score: 0', { font: '14px Arial', fill: 'black' });
+        scoreText.fixedToCamera = true;
+
+
+        /*
+            Code for the pause menu
+        */
+
+        // Create a label to use as a button
+        pause_label = game.add.text(w - 80, 15, 'Pause', { font: '16px Arial', fill: '#fff' });
+        pause_label.fixedToCamera = true;
+        pause_label.inputEnabled = true;
+        pause_label.events.onInputUp.add(function () {
+            // When the pause button is pressed, we pause the game
+            game.paused = true;
+
+            // Then add the menu
+            menu = game.add.sprite(w/2, h/2, 'menu');
+            menu.anchor.setTo(0.5, 0.5);
+
+
+            // And a label to illustrate which menu item was chosen. (This is not necessary)
+            choiseLabel = game.add.text(w/2, h-150, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
+            choiseLabel.anchor.setTo(0.5, 0.5);
+
+        });
+
+        // Add a input listener that can help us return from being paused
+        game.input.onDown.add(unpause, self);
+
+        // And finally the method that handels the pause menu
+        function unpause(event){
+            // Only act if paused
+            if(game.paused){
+                // Calculate the corners of the menu
+                var x1 = w/2 - 270/2, x2 = w/2 + 270/2,
+                    y1 = h/2 - 180/2, y2 = h/2 + 180/2;
+
+                // Check if the click was inside the menu
+                if(event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2 ){
+                    // The choicemap is an array that will help us see which item was clicked
+                    var choisemap = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+                    // Get menu local coordinates for the click
+                    var x = event.x - x1,
+                        y = event.y - y1;
+
+                    // Calculate the choice
+                    var choise = Math.floor(x / 90) + 3*Math.floor(y / 90);
+
+                    // Display the choice
+                    choiseLabel.text = 'You chose menu item: ' + choisemap[choise];
+                }
+                else{
+                    // Remove the menu and the label
+                    menu.destroy();
+                    choiseLabel.destroy();
+
+                    // Unpause the game
+                    game.paused = false;
+                }
+            }
+        };
+
+
+
+
     }
+
+
+
+
+
+
+
 
     function update() {
 
@@ -122,6 +213,12 @@
 
         // Removes the star from the screen
         coin.kill();
+
+        //  Add and update the score
+        score += 10;
+        scoreText.text = 'Score: ' + score;
+
+        console.log(score);
 
       }
 
